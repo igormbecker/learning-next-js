6db406918cb937fef634ddc156eefd5b919957f2
Igor Becker <igor.becker@smlbrasil.com.br>
2021-04-09 09:23:02 -0300

    Adicionando o projeto novamente

---
+++ Pages/index.js
@@ -0,0 +1,38 @@
+﻿import { useState } from "react";
+
+function Home(){
+    return (
+        <>
+        <div style={{backgroundColor: "gray"}}>
+            <h2>Home</h2>
+            <Counter />
+        </div>
+        <Footer />
+        </>
+    )
+}
+
+function Counter(){
+    const [counter,setCounter] = useState(1);
+
+    function addCounter() {
+        setCounter(counter + 1);
+    }
+
+    return (
+        <div>
+            <div>{counter}</div>
+            <button onClick={addCounter}>Add</button>
+        </div>
+    )
+}
+
+function Footer(){
+    return (
+        <div className="cFooter">
+            Footer Here
+        </div>
+    )
+}
+
+export default Home;
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # learning-next-js
+
+# Instalar pacote json (-y aceitando todas opçoes padrões).
+npm init -y
+
+# Instalar dependência next.js .
+npm install next
+
+# Instalar dependência react.
+npm install react
+
+# Instalar dependência react framework.
+npm install react-dom
+
+# Instalar tudo com apenas um Comando
+npm install next react react-dom
+
+# Comando para iniciar o projeto
+npm run dev
+
+# Comando para buildar o projeto
+rpm run build
+
+# Comando para atualizar o npm.
+sudo npm install -g npm
